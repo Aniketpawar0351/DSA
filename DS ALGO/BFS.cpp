c98f3be29c81ef406dfca7dd743dbb0125cd99dd
Aniket <aniketpawar0351@gmail.com>
2022-02-28 19:28:27 +0530

    Initial Commit

---
+++ DS ALGO/BFS.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+typedef pair<int,int> p;
+
+queue<int> q;
+void bfs(int src)
+{
+
+}
+int main()
+{
+    int v,e;
+    cout<<"enter vertices number: ";
+    cin>>v;
+    cout<<"enter edges: ";
+    cin>>e;
+
+    vector<vector<int>>v1[10000];
+    int temp1,temp2;
+    for(int i=0;i<e;i++)
+    {
+        cin>>temp1>>temp2;
+        v1[temp1].push_back(temp2);
+        v1[temp2].push_back(temp1);
+
+    }
+    bfs(0);
+    return 0;
+}
+
